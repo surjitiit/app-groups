4c220513bc7b8cec471468c1f08ca3bc0bfaced1
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-03-20 21:05:32 +0000

    - Added some group policy translations

--- language/en_US/groups_lang.php
+++ language/en_US/groups_lang.php
@@ -1,5 +1,6 @@
 <?php
 
+$lang['groups_add_user_defined_group'] = 'Add User Defined Group';
 $lang['groups_app_description'] = 'The groups app allows an administrator to create, delete and modify groups on the system.  Groups can be used to efficiently manage permissions in accessing server resources.';
 $lang['groups_app_name'] = 'Groups';
 $lang['groups_app_policies'] = 'App Policies';
@@ -8,8 +9,8 @@ $lang['groups_description'] = 'Description';
 $lang['groups_description_invalid'] = 'Description is invalid.';
 $lang['groups_edit_members'] = 'Edit Members';
 $lang['groups_global_policy'] = 'Global Policy';
-$lang['groups_group'] = 'Group';
 $lang['groups_group_already_exists'] = 'Group already exists.';
+$lang['groups_group'] = 'Group';
 $lang['groups_group_id'] = 'Group ID';
 $lang['groups_group_is_reserved_for_system'] = 'Group is reserved for the system.';
 $lang['groups_group_is_reserved_for_windows'] = 'Group is reserved for Windows integration.';
@@ -20,8 +21,11 @@ $lang['groups_group_name_invalid'] = 'Group name is invalid.';
 $lang['groups_group_not_found'] = 'Group not found.';
 $lang['groups_groups'] = 'Groups';
 $lang['groups_members'] = 'Members';
+$lang['groups_no_user_defined_groups_warning'] = 'All the user defined groups have a configured policy.  If you would like to add a new policy, please add more groups.';
 $lang['groups_plugin_groups'] = 'Plugin Groups';
+$lang['groups_policy_name_invalid'] = 'Policy name is invalid.';
 $lang['groups_policy_name'] = 'Policy Name';
+$lang['groups_policy'] = 'Policy';
 $lang['groups_spaces_not_allowed_in_group_names'] = 'Spaces not allowed in group names.';
 $lang['groups_tooltip'] = 'Though not explicitly shown here, the allusers group includes all users on the system.';
 $lang['groups_user_defined_groups'] = 'User Defined Groups';
